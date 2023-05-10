442c9bacb01839e6ed51e463287efed1b86adacb
suman <sumandari@mediacatch.io>
2023-05-10 14:43:22 +0200

    refactored default get value

--- src/mediacatch_s2t/__init__.py
+++ src/mediacatch_s2t/__init__.py
@@ -7,34 +7,25 @@ __version__ = "0.0.4"
 
 import os
 
-URL = (
-    os.environ.get('MEDIACATCH_URL') or
-    'https://s2t.mediacatch.io'
-)
+URL: str = os.environ.get('MEDIACATCH_URL', 'https://s2t.mediacatch.io')
+SINGLE_UPLOAD_ENDPOINT: str = os.environ.get(
+    'MEDIACATCH_PRESIGN_ENDPOINT',
+    '/presigned-post-url')
+MULTIPART_UPLOAD_CREATE_ENDPOINT: str = os.environ.get(
+    'MEDIACATCH_MULTIPART_UPLOAD_CREATE_ENDPOINT',
+    '/multipart-upload/id')
+MULTIPART_UPLOAD_URL_ENDPOINT: str = os.environ.get(
+    'MEDIACATCH_MULTIPART_UPLOAD_URL_ENDPOINT',
+    '/multipart-upload/url')
+MULTIPART_UPLOAD_COMPLETE_ENDPOINT: str = os.environ.get(
+    'MEDIACATCH_MULTIPART_UPLOAD_COMPLETE_ENDPOINT',
+    '/multipart-upload/complete')
+UPDATE_STATUS_ENDPOINT: str = os.environ.get(
+    'MEDIACATCH_UPDATE_STATUS_ENDPOINT',
+    '/upload-completed')
+TRANSCRIPT_ENDPOINT: str = os.environ.get(
+    'MEDIACATCH_TRANSCRIPT_ENDPOINT', '/result')
+PROCESSING_TIME_RATIO: float = 0.1
+MULTIPART_FILESIZE: int = 1 * 1024 * 1024 * 1024
 
-SINGLE_UPLOAD_ENDPOINT = (
-    os.environ.get('MEDIACATCH_PRESIGN_ENDPOINT') or
-    '/presigned-post-url'
-)
-MULTIPART_UPLOAD_CREATE_ENDPOINT = (
-    os.environ.get('MEDIACATCH_MULTIPART_UPLOAD_CREATE_ENDPOINT') or
-    '/multipart-upload/id'
-)
-MULTIPART_UPLOAD_URL_ENDPOINT = (
-    os.environ.get('MEDIACATCH_MULTIPART_UPLOAD_URL_ENDPOINT') or
-    '/multipart-upload/url'
-)
-MULTIPART_UPLOAD_COMPLETE_ENDPOINT = (
-    os.environ.get('MEDIACATCH_MULTIPART_UPLOAD_COMPLETE_ENDPOINT') or
-    '/multipart-upload/complete'
-)
-UPDATE_STATUS_ENDPOINT = (
-    os.environ.get('MEDIACATCH_UPDATE_STATUS_ENDPOINT') or
-    '/upload-completed'
-)
-TRANSCRIPT_ENDPOINT = (
-    os.environ.get('MEDIACATCH_TRANSCRIPT_ENDPOINT') or
-    '/result'
-)
-PROCESSING_TIME_RATIO = 0.1
-MULTIPART_FILESIZE = 1 * 1024 * 1024 * 1024
+ENABLE_AUTOMATIC_UPDATE: bool = True
